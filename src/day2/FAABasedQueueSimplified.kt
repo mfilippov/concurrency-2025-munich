ffdcca847307aec98add72036f957ec1d075a916
Mikhail Filippov <mikhail.filippov@jetbrains.com>
2025-07-23 12:01:13 +0200

    Task FAABasedQueueSimplified

--- src/day2/FAABasedQueueSimplified.kt
+++ src/day2/FAABasedQueueSimplified.kt
@@ -2,6 +2,7 @@ package day2
 
 import day1.*
 import java.util.concurrent.atomic.*
+import kotlin.concurrent.atomics.fetchAndIncrement
 import kotlin.math.*
 
 class FAABasedQueueSimplified<E> : Queue<E> {
@@ -10,26 +11,43 @@ class FAABasedQueueSimplified<E> : Queue<E> {
     private val deqIdx = AtomicLong(0)
 
     override fun enqueue(element: E) {
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = enqIdx.get()
-        enqIdx.set(i + 1)
-        // TODO: Atomically install the element into the cell
-        // TODO: if the cell is not poisoned.
-        infiniteArray.set(i.toInt(), element)
+        while (true) {
+            // TODO: Increment the counter atomically via Fetch-and-Add.
+            // TODO: Use `getAndIncrement()` function for that.
+            val i = enqIdx.andIncrement
+            // TODO: Atomically install the element into the cell
+            // TODO: if the cell is not poisoned.
+            if (infiniteArray.compareAndSet(i.toInt(), null, element)) {
+                return
+            }
+        }
+    }
+
+    private fun isEmpty(): Boolean {
+        while (true) {
+            val deq = deqIdx.get()
+            val enq = enqIdx.get()
+            if (deq != deqIdx.get()) continue
+            return enq <= deq
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        // Is this queue empty?
-        if (enqIdx.get() <= deqIdx.get()) return null
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = deqIdx.get()
-        deqIdx.set(i + 1)
-        // TODO: Try to retrieve an element if the cell contains an
-        // TODO: element, poisoning the cell if it is empty.
-        return infiniteArray.get(i.toInt()) as E
+        while (true) {
+            // Is this queue empty?
+            if (isEmpty()) return null
+            // TODO: Increment the counter atomically via Fetch-and-Add.
+            // TODO: Use `getAndIncrement()` function for that.
+            val i = deqIdx.andIncrement
+
+            val value = infiniteArray.getAndSet(i.toInt(), POISONED)
+            if (value != null) {
+                return value as E
+            }
+            // TODO: Try to retrieve an element if the cell contains an
+            // TODO: element, poisoning the cell if it is empty.
+        }
     }
 
     override fun validate() {
